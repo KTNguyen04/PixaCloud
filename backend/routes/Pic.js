dfe688bf56e296fe001a707f77e1020b5203cc28
KTNguyen04 <championvi12@gmail.com>
2025-02-17 22:36:25 +0700

    crud api

--- backend/routes/Pic.js
+++ backend/routes/Pic.js
@@ -4,7 +4,7 @@ const verifyToken = require("../middlewares/VerifyToken");
 const Pic = require("../controllers/Pic");
 
 router.get("/", Pic.getAll);
-router.get("/personal", verifyToken, Pic.getAll);
+router.get("/personal", verifyToken, Pic.getAllByAuthor);
 router.post("/", verifyToken, Pic.upload.single("pic"), Pic.create);
 router.delete("/:id", verifyToken, Pic.delete);
 router.put("/:id", verifyToken, Pic.update);
