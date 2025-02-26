14748a8940ff1351b1d9d4c06135d6eb0fa9a597
KTNguyen04 <championvi12@gmail.com>
2025-02-27 01:26:31 +0700

    edit models

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,6 +2,6 @@ import apiAuth from "./apiAuth";
 
 export default {
   async googleLogin(credentials) {
-    return await apiAuth.post("/auth/google", credentials);
+    return await apiAuth.post("/auth", credentials);
   },
 };
